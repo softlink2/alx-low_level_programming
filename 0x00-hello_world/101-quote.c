2d8beb3364a02b6b22ad1d3f1ce783fa53c5d1d7
Muhammed Abiola <hi@mabiola.net>
2023-03-16 23:26:39 +0100

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * Return: ALWAYS 1
+ */
+
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, s, strlen(s));
+	return (1);
+}
