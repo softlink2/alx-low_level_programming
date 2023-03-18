cfab8443f3b4e2d4da32378b18eb7af15650f428
Muhammed Abiola <hi@mabiola.net>
2023-03-18 04:00:29 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 value on success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
