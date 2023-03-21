e0b780c65847f65adb1dd8eb04a101987bbfc818
Muhammed Abiola <hi@mabiola.net>
2023-03-21 11:19:07 +0100

    print alphabet x10 - including the original function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,3 +13,19 @@ void print_alphabet_x10(void)
 		print_alphabet();
 	return;
 }
+
+/**
+ * print_alphabet - print an alphabet, in lowercase, followed by a newline
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+
+	return;
+}
