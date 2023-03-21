4a8226dd5ee082112811a848067ad77a49099b89
Muhammed Abiola <hi@mabiola.net>
2023-03-21 23:04:16 +0100

    fix: sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		putchar('-');
 		return (-1);
 	}
 	
-	printf("0");
+	putchar('0');
 	return (0);
 }
