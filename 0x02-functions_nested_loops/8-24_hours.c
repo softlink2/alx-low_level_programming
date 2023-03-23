80477c140582ff2ca73400ff813c90e8cc74b85e
Muhammed Abiola <hi@mabiola.net>
2023-03-23 07:21:23 +0100

    fix: 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,32 +10,32 @@ void jack_bauer(void)
 
 	while (1)
 	{
-			if (time[3] > 9)
-			{ 
-				time[3] = 0;
-				time[2]++;
-			}
+		if (time[3] > 9)
+		{ 
+			time[3] = 0;
+			time[2]++;
+		}
 
-			if (time[2] > 5)
-			{
-				time[2] = 0;
-				time[1]++;
-			}
+		if (time[2] > 5)
+		{
+			time[2] = 0;
+			time[1]++;
+		}
 
-			if (time[1] > 9)
-			{
-				time[1] = 0;
-				time[0]++;
-			}
-			
-			if (time[0] == 2 && time[1] == 4)
-				break;
-			_putchar(time[0] + '0');
-			_putchar(time[1] + '0');
-			_putchar(':');
-			_putchar(time[2] + '0');
-			_putchar(time[3] + '0');
-			_putchar('\n');
-			time[3]++;
+		if (time[1] > 9)
+		{
+			time[1] = 0;
+			time[0]++;
+		}
+
+		if (time[0] == 2 && time[1] == 4)
+			break;
+		_putchar(time[0] + '0');
+		_putchar(time[1] + '0');
+		_putchar(':');
+		_putchar(time[2] + '0');
+		_putchar(time[3] + '0');
+		_putchar('\n');
+		time[3]++;
 	}
 }
