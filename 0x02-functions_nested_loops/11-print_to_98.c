8fc88f4a1c70cff7af1126b9926aa193d35cf82f
Muhammed Abiola <hi@mabiola.net>
2023-03-23 04:15:49 +0100

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,23 @@
 
 void print_to_98(int n)
 {
+	/*
+		int step = n >= 98 ? -1 : 1;
+		for (; n != 98; n += step) {
+			printf("%d%s", n, n != 98 ? ", " : "");
+		}
+		printf("98\n");
+	*/
+
 	if (n >= 98)
 		for (; n >= 98; n--)
-			n > TO98 ? printf("%d, ", n) : printf("%d", n);	
+			printf("%d%s", n, n > TO98 ? ", " : "");
 	else
 		for (; n <= TO98; n++)
-			n < TO98 ? printf("%d, ", n) : printf("%d", n);
+			printf("%d%s", n, n < TO98 ? ", " : "");
 	printf("\n");
 }
+
+/*
+
+*/
