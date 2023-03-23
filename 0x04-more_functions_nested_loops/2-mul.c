0e0ab3b7b2f77c0287607a999b0a6ea140a5a599
Muhammed Abiola <hi@mabiola.net>
2023-03-23 07:15:03 +0100

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: integer
+ * @b: integer
+ * Return: result of multiplying a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H_
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 #endif
