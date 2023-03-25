195e54511bf48c0e0df922b284253357c5118845
Muhammed Abiola <hi@mabiola.net>
2023-03-25 08:29:39 +0100

    fix: more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 		for (n = 0x0; n < 0xf; n++)
 		{
 			if (n >= 0xA)
-			{
 				_putchar(n / 0xA + '0');
-				_putchar(n % 0xA + '0');
-			}
-			else
-				_putchar(n + '0');
+			_putchar(n % 0xA + '0'); 
 		}
 		_putchar('\n');
 	}
