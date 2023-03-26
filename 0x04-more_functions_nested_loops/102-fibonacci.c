a587a6a6719661a3f88c2de51efd2586d274f340
Muhammed Abiola <hi@mabiola.net>
2023-03-26 04:29:26 +0100

    style: fibonacci

--- 0x04-more_functions_nested_loops/102-fibonacci.c
+++ 0x04-more_functions_nested_loops/102-fibonacci.c
@@ -3,6 +3,7 @@
 /**
  * main - entry point
  * Description: print the first 50 fibonacci numbers, starting with 1 and 2
+ * Return: 0
 */
 
 int main(void)
@@ -13,9 +14,10 @@ int main(void)
     /*
      * `fib` is an array used to store the two numbers each Fibonacci
      * sequence. `fib[0]` stores the current number, and `fib[1]` stores the
-     * previous number. The next number is computed as the sum of `fib[0]`
-     * and `fib[1]`, and stored in `fib[2]`. Then, `fib[0]` is set to `fib[1]`,
-     * and `fib[1]` is set to `fib[2]`, so that the next number can be generated.
+     * previous number.
+	 * The next number is computed as the sum of `fib[0]`
+	 * and `fib[1]`, and stored in `fib[2]`.
+	 * `fib[0]` is set to `fib[1]`, and `fib[1]` is set to `fib[2]`
      */
 
 	while (n++ < 50)
