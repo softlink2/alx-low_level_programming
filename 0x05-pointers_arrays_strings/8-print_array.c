d60296c9ef4a63766ceefb10a5df3ff303ed1ba0
Muhammed Abiola <hi@mabiola.net>
2023-03-28 10:52:57 +0100

    Add function to print an array of integers with a newline at the end

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of an array of integers, followed by a
+ * newline
+ * @n: number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		printf("%d", a[c]);
+		if (c < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
