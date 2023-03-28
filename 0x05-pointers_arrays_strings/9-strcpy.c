fe5fce2efbaf84ce1b40651c8925a109e8625332
Muhammed Abiola <hi@mabiola.net>
2023-03-28 22:51:29 +0100

    Add implementation for _strcpy function which copies the string pointed to by src, including the terminating null byte, to the buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy the string pointed to by src, including the terminating null
+ * byte, to the buffer pointed to by dest
+ * @src: string to copy
+ * @dest: buffer to copy to
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	unsigned int c;
+
+	for (c = 0; c < strlen(src); c++)
+		dest[c] = src[c];
+	dest[c] = '\0';
+
+	return (dest);
+}
