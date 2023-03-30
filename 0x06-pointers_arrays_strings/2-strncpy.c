829c976d58f84d223fa7a9ea011a7dd475913cd3
Muhammed Abiola <hi@mabiola.net>
2023-03-30 03:34:45 +0100

    Add function to copy a string up to a certain number of bytes with _strncpy()

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int e;
+
+	for (e = 0; src[e] != '\0' && e < n; e++)
+		dest[e] = src[e];
+	dest[e] = '\0';
+
+	return (dest);
+}
