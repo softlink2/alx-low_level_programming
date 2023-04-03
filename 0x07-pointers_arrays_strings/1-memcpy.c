fff97bc12f6528b704757bf78618fd066112a76c
Muhammed Abiola <hi@mabiola.net>
2023-04-03 16:44:15 +0100

    Add _memcpy function and main program to test it

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy n bytes from memory area src to memory area dest
+ * @dest: destination memory region
+ * @src: source memory region
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int d;
+
+	for (d = 0; d < n; d++)
+		dest[d] = src[d];
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #ifndef _MAIN_H_
 char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
 #endif
