d688ce3d40c1c5ca38a4b13d06360679effcd87d
Muhammed Abiola <hi@mabiola.net>
2023-04-11 17:46:19 +0100

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "main.h"
 
 char **
 strtow(char *str)
@@ -29,37 +30,3 @@ strtow(char *str)
     return (ptr);
 }
 
-/**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
- */
-void print_tab(char **tab)
-{
-    int i;
-
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
-}
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
-{
-    char **tab;
-
-    tab = strtow("      ALX School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
-}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+char **strtow(char *);
+void print_tab(char **);
+#endif
