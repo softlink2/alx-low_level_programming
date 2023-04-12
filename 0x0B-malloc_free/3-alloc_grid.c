b7c5f9c8d10455437c3dc6cf3c9c366dd54a3883
Muhammed Abiola <hi@mabiola.net>
2023-04-12 02:23:08 +0100

    Refactor alloc_grid function to handle failed mallocs

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,31 +9,32 @@
  * Return: a two dimensional grid
  */
 
-int **
-alloc_grid(int width, int height)
+int **alloc_grid(int width, int height)
 {
-  int **grid;
-  int c, d;
+	int **grid;
+	int c, d;
 
-  if (width <= 0 || height <= 0)
-    return (NULL);
-  grid = malloc(sizeof(int *) * height);
-  if (grid == NULL)
-    return (NULL);
-  for (c = 0; c < height; c++)
-  {
-    grid[c] = malloc(sizeof(int) * width);
-    if (grid[c] == NULL)
-    {
-      for (d = 0; d < c; d++)
-      {
-        free(grid[d]);
-        grid[d] = NULL;
-      }
-      free(grid);
-      grid = NULL;
-    }
-  }
-  
-  return (grid);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (c = 0; c < height; c++)
+	{
+		grid[c] = malloc(sizeof(int) * width);
+		if (grid[c] == NULL)
+		{
+			for (d = 0; d < c; d++)
+			{
+				free(grid[d]);
+				grid[d] = NULL;
+			}
+
+			free(grid);
+			grid = NULL;
+		}
+	}
+
+	return (grid);
 }
