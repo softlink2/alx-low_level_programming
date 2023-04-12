03b6007602c3bfedc110428793d5e86368d5dd76
Muhammed Abiola <hi@mabiola.net>
2023-04-12 02:19:39 +0100

    Refactor print_tab and main functions for readability and clarity

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -5,17 +5,14 @@
 /**
  * print_tab - Prints an array of string
  * @tab: The array to print
- *
- * Return: nothing
  */
+
 void print_tab(char **tab)
 {
-    int i;
+	int i;
 
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
+	for (i = 0; tab[i] != NULL; ++i)
+		printf("%s\n", tab[i]);
 }
 
 /**
@@ -23,16 +20,19 @@ void print_tab(char **tab)
  *
  * Return: 1 if an error occurred, 0 otherwise
  */
+
 int main(void)
 {
-    char **tab;
+	char **tab;
 
-    tab = strtow("      ALX School         #cisfun      ");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
+	tab = strtow("      ALX School         #cisfun      ");
+	if (tab == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+
+	print_tab(tab);
+
+	return (0);
 }
