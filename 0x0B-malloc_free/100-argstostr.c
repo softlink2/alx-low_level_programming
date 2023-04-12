0a192b7335866b842002e5cc13df2cd56fa395eb
Muhammed Abiola <hi@mabiola.net>
2023-04-12 02:20:30 +0100

    Refactor argstostr function to handle edge cases and improve readability

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,11 +9,14 @@ argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (c = 0; c < ac; c++)
 		d += strlen(av[c]) + 1;
+
 	ptr = malloc(d + 1);
 	if (ptr == NULL)
 		return (NULL);
+
 	for (c = 0; c < ac; c++)
 	{
 		for (d = 0; av[c][d]; d++)
@@ -21,8 +24,8 @@ argstostr(int ac, char **av)
 		if (av[c][d] == '\0')
 			ptr[e++] = '\n';
 	}
+
 	ptr[e] = '\0';
 
 	return (ptr);
 }
-
