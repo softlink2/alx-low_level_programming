261caf228c58c5c7af58e2a9d2ddcbcfb08863ca
Muhammed Abiola <hi@mabiola.net>
2023-04-18 08:00:33 +0100

    Add documentation to print_chessboard() function in 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-void
-print_chessboard(char (*a)[8])
+/**
+* print_chessboard - print a chess board
+* @a: chessboard
+*/
+
+void print_chessboard(char (*a)[8])
 {
 	int c, d;
 
-  /* Thinking of other ways to do this that don't use magic numbers */
-  
 	for (c = 0; c < 8; c++)
 	{
 		for (d = 0; d < 8; d++)
@@ -17,3 +19,4 @@ print_chessboard(char (*a)[8])
 		putchar('\n');
 	}
 }
+
