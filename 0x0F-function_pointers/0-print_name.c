7755a12d8d8e2b6fd91f35c8f8804aab56917d41
Muhammed Abiola <hi@mabiola.net>
2023-04-19 17:24:49 +0100

    Add function prototypes to header file in 0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: function to call
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    (*f)(name);
+}
