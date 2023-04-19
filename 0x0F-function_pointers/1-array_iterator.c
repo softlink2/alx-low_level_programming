b0a33154c50806bd4eac7f9fb8f546a4a5e1b451
Muhammed Abiola <hi@mabiola.net>
2023-04-19 17:41:02 +0100

    Add array_iterator function to execute a function on each element of an array in 0x0F-function_pointers directory

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function given as a parameter on each element
+ * of an array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t c;
+
+	for (c = 0; c < size; c++)
+		(*action)(array[c]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdio.h>
 void print_name(char *, void (*)(char *));
+void array_iterator(int *, size_t, void (*)(int));
 #endif
